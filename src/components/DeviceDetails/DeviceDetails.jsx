d390dfba7a83ffc4fdc7305681aa798d756c15c4
mehu-hub <designwithmehedi@gmai.com>
2023-11-22 01:19:06 +0600

    Props

---
+++ src/components/DeviceDetails/DeviceDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DeviceDetails = (props) => {
+    return (
+        <div>
+            <p>It's Price is: {props.price}</p>
+
+        </div>
+    );
+};
+
+export default DeviceDetails;<p>Information About Your Device</p>
