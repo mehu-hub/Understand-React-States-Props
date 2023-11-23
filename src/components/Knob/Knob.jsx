91ae61134ff2e8ede2d2a68327ff02cb37b74cc8
mehu-hub <designwithmehedi@gmai.com>
2023-11-23 10:17:15 +0600

    Added Content

--- src/components/Knob/Knob.jsx
+++ src/components/Knob/Knob.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Knob = (props) => {
     return (
         <div style={{border:"2px solid green", margin:"20px", padding:"20px"}}>
-            <h2>knoc Components</h2>
+            <h2>knob Components</h2>
             <p>Steps is here: {props.steps}</p>
         </div>
     );
